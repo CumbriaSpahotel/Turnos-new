a04b408ca8df3743e22734c77ebb70bed0152dc4
https://github.com/CumbriaSpahotel <comunicaciones@hotelguadiana.es>
2026-08-06 20:36:31 +0200

    Remove scratch and brain temporary files from git index

--- scratch/check_chars.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('admin.js', 'utf8');
-const lines = content.split('\n');
-for (let i = 5635; i < 5645; i++) {
-    console.log(i + ': ' + JSON.stringify(lines[i]));
-}
--- scratch/test_intercambio.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-const empId = "proximamente";
-const resolvedA = "dani";
-const resolvedB = "proximamente";
-
-console.log("isOrigin:", empId === resolvedA);
-console.log("isDestination:", empId === resolvedB);
